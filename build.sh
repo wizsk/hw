dc67432ea8db088b11e02ab4f83a081be1ea5b48
Sakib <sakibul706@gmail.com>
2024-11-08 22:50:51 +0600

    comp

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ set -e
 GOOS=linux CGO_ENABLED=0 go build -ldflags "-s -w" -o build/
 GOARCH=arm64 GOOS=linux go build -ldflags "-s -w" -o build/hw_arm
 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o build/
+
+[ "$1" = "c" ] || exit
+cd build || exit
+
+tar -czvf hw_Linux_aarch64.tar.gz hw_arm
+tar -czvf hw_Linux_x86_64.tar.gz hw
+zip -r hw_windows_x86_64.zip hw.exe
+
+
