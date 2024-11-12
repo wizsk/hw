039747130129a1fb0d9d5cd755c6c07fdb0d2eb6
Sakib <sakibul706@gmail.com>
2024-11-12 15:18:53 +0600

    win install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,17 @@
 $ErrorActionPreference = "Stop"  # Stop on errors
 
 
-# # Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name $progName -ErrorAction SilentlyContinue
-# Write-Host "$programName removed from startup for all users."
-#
-# return
-# # Add registry key for all users (requires admin privileges)
-# Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name $progName -Value $hwPath
-# Write-Host "$progName added to startup for all users."
+$progName = "HansWehrsDictionary"
+$programDataPath = [System.Environment]::GetFolderPath('ProgramFiles')
+$hwPath = Join-Path -Path $programDataPath -ChildPath $progName
+
+# Define the URL of the file and the path where you want to save it
+$icoUrl = "https://raw.githubusercontent.com/wizsk/hw/refs/heads/main/assets/pub/hw.ico"
+$url = "https://github.com/wizsk/hw/releases/latest/download"
+$sysTmp = [System.IO.Path]::GetTempPath()
+$file = "hw_windows_x86_64.zip"
+$downFilePath = Join-Path -Path $sysTmp -ChildPath $file
+$icoPath = Join-Path -Path $hwPath -ChildPath "hw.ico"
 
 # Check if the folder exists
 if (-Not (Test-Path -Path $hwPath)) {
@@ -19,9 +23,9 @@ if (-Not (Test-Path -Path $hwPath)) {
 
 try {
     Write-Host "Downloading: $url/$file"
-    # Invoke-WebRequest -Uri "$url/$file" -OutFile $downFilePath
+    Invoke-WebRequest -Uri "$url/$file" -OutFile $downFilePath
     Write-Host "Downloading: $icoUrl"
-    # Invoke-WebRequest -Uri "$icoUrl" -OutFile $icoPath
+    Invoke-WebRequest -Uri "$icoUrl" -OutFile $icoPath
 }
 catch {
     Write-Host "Err: Could Downlaoing failed! bye"
@@ -63,42 +67,35 @@ if ($systemPath -notlike "*$hwPath*") {
 # Removing files
 try {
     Remove-Item -Path "$exFolder" -Recurse
-    # Remove-Item -Path $downFilePath
+    Remove-Item -Path $downFilePath
 
 }
 catch {}
 
 
-$progName = "HansWehrsDictionary"
-$programDataPath = [System.Environment]::GetFolderPath('ProgramFiles')
-$hwPath = Join-Path -Path $programDataPath -ChildPath $progName
-
-# Define the URL of the file and the path where you want to save it
-$icoUrl = "https://raw.githubusercontent.com/wizsk/hw/refs/heads/main/assets/pub/hw.ico"
-$url = "http://10.0.0.110:8001"
-# $url = "https://github.com/wizsk/hw/releases/latest/download"
-$sysTmp = [System.IO.Path]::GetTempPath()
-$file = "hw_windows_x86_64.zip"
-$downFilePath = Join-Path -Path $sysTmp -ChildPath $file
-$icoPath = Join-Path -Path $hwPath -ChildPath "hw.ico"
-
-
-# Arguments for the executable (if any)
+# adding desktop shorcut
 $arguments = ""
-# Get the path to the current user's Desktop
 $desktopPath = [System.IO.Path]::Combine($env:USERPROFILE, "Desktop")
-# Path to save the shortcut
 $shortcutPath = [System.IO.Path]::Combine($desktopPath, "$progName.lnk")
-# Create a WScript.Shell COM object to create the shortcut
 $wshShell = New-Object -ComObject WScript.Shell
 $shortcut = $wshShell.CreateShortcut($shortcutPath)
-# Set the target executable and arguments
 $hwExeFullPath = Join-Path -Path $hwPath -ChildPath "hw.exe"
 $shortcut.TargetPath = $hwExeFullPath
 $shortcut.Arguments = $arguments
 $shortcut.IconLocation = $icoPath
-# Save the shortcut
 $shortcut.Save()
+
+# adding desktop shartmenu for search
+$arguments = ""
+$startMenuPath = [System.IO.Path]::Combine($env:ProgramData, "Microsoft\Windows\Start Menu\Programs")
+$shortcutPath = [System.IO.Path]::Combine($startMenuPath, "$progName.lnk")
+$wshShell = New-Object -ComObject WScript.Shell
+$shortcut = $wshShell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $hwExeFullPath
+$shortcut.Arguments = $arguments
+$shortcut.IconLocation = $icoPath
+$shortcut.Save()
+Write-Host "Shortcut created in the Start Menu for all users. It will appear in Windows Search."
 
 $shortcutPath = [System.IO.Path]::Combine($hwPath, "$progName.lnk")
 $wshShell = New-Object -ComObject WScript.Shell
@@ -110,7 +107,6 @@ $shortcut.Arguments = $arguments
 $shortcut.IconLocation = $icoPath
 # Save the shortcut
 $shortcut.Save()
-Write-Host "Shortcut created on Desktop."
 
 Write-Host ""
 Write-Host "Installation compleaded! Now run 'hw'"
